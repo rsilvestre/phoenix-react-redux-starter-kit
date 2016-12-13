f43e39e5285320371e562e7d99dbad2c345aec92
Michael Silvestre <willtard@gmail.com>
2016-12-13 09:43:13 +0100

    feature(travis): add better travis support

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :phoenix_react_redux_starter_kit, PhoenixReactReduxStarterKit.Endpoint,
+config :phoenix_trello, PhoenixReactReduxStarterKit.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -13,11 +13,7 @@ config :logger, level: :warn
 config :phoenix_trello, PhoenixReactReduxStarterKit.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "phoenix_react_redux_starter_kit_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
-
-# Guardian configuration
-config :guardian, Guardian,
-  secret_key: "W9cDv9fjPtsYv2gItOcFb5PzmRzqGkrOsJGmby0KpBOlHJIlhxMKFmIlcCG9PVFQ"
