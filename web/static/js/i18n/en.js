4b6c23f8fc79165504f91e7398be72c4fe04ffb7
Michael Silvestre <willtard@gmail.com>
2016-12-15 10:08:51 +0100

    Revert "Merge branch 'language-support' into master"
    This reverts commit 9914ca63052521f097277f2053ef9e0c93310803, reversing
    changes made to 74acac8b02cd0470c6aca381633d2f1c0b8f8f83.

--- web/static/js/i18n/en.js
+++
@@ -1,8 +0,0 @@
-/*eslint-disable max-len,quotes*/
-export default {
-  "home.welcome": "Welcome to the Phoenix React Redux Starter Kit",
-  "home.sampleCounter": "Sample Counter:",
-  "languageSelector.spanish": "Spanish",
-  "languageSelector.english": "English",
-  "languageSelector.french": "French"
-}
--- web/static/js/layouts/CoreLayout/CoreLayout.js
+++ web/static/js/layouts/CoreLayout/CoreLayout.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../../components/Header'
-import LanguageSelectorController from '../../containers/LanguageSelectorController'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
@@ -10,7 +9,6 @@ export const CoreLayout = ({ children }) => (
     <div className='core-layout__viewport'>
       {children}
     </div>
-    <LanguageSelectorController />
   </div>
 )
 
