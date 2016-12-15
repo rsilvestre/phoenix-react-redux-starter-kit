570143b7dddd45e9b9599f8d560bd26ac18289b4
Michael Silvestre <willtard@gmail.com>
2016-12-15 10:08:51 +0100

    Revert "Merge branch 'language-support' into master"
    This reverts commit 9914ca63052521f097277f2053ef9e0c93310803, reversing
    changes made to 74acac8b02cd0470c6aca381633d2f1c0b8f8f83.

--- web/static/js/i18n/es.js
+++
@@ -1,8 +0,0 @@
-/*eslint-disable max-len,quotes*/
-export default {
-  "home.welcome": "Bienvenido al Starter Kit de Phoenix React Redux",
-  "home.sampleCounter": "Contador de ejemplo:",
-  "languageSelector.spanish": "Español",
-  "languageSelector.english": "Inglés",
-  "languageSelector.french": "Francés"
-}
