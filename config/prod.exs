1e62fa08cfe58407f74313b3bd0a593b2c001c01
Michael Silvestre <willtard@gmail.com>
2016-12-28 23:16:49 +0100

    fix heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_react_redux_starter_kit, PhoenixReactReduxStarterKit.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "frozen-beach-38768.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
